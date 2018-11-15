a02db209229595cf884c464d11a3162f49be352e
saranyamohandas <saranyamohandas2@gmail.com>
2018-11-14 21:01:20 -0800

    final changes

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # unit-4-game
-unit-4-game - Challenger and defenders
+
+---
+
+Homework #3B -unit-4-game - Challenger and defenders
+___
+
+### This is a interactive Game of thrones RPG game application that has been developed using HTML,CSS,bootstrap,javascript and jquery library.
+
+### Overview of the application -
+* This is a single player Role-playing game. 
+* Given choice of four characters,player chooses his character and rest of them becomes his enemies.
+* Player gets to choose his defended one by one until player defeats all available enemies.
+* When player defeats all enemies,player wins the game.
+* Both player and enemies are given initial Health points and attack points.
+* Health points & attack points varies depending upon the chosen character.
+* When player attacks enemy his health points decreases by enemy's attack point and vice versa.
+* Player attack points get incremented by his base attack points for every attack but enemy's counter attack point remains constant throughout the game.
+
+
+### Overview of the script -
+* Once the player clicks 'Start' button, the game starts.
+* Application has used jquery extensively for adding,modifying and removing events
+* JQuery has also been used to add styles and dynamically generate elements.
+
+__Link to game__ : https://saranyamohandas.github.io/unit-4-game/
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,7 @@
 $(document).ready(function(){
-//    var combatPoints = [{name : 'avatar1',hp:100,ap:20},
-//                         {name :'avatar2', hp:200,ap:20},{name:'avatar3',hp:350,ap:30},
-//                         {name: 'avatar4',hp:500,ap:25}];
+
     var combatPoints = [{name : 'John Snow',hp:100,ap:25},
-                         {name :'Cersei', hp:200,ap:20},{name:'Daenerys',hp:350,ap:30},
+                         {name :'Cersei', hp:200,ap:20},{name:'Daenerys',hp:350,ap:25},
                          {name: 'White Walker',hp:500,ap:20}];
     var challenger = false;
     var defender = false;
@@ -15,13 +13,17 @@ $(document).ready(function(){
     var getDefenderId;
     var getChallengerpt = {};
     var getDefenderpt = {};
-    var getDefenderName,getDefenderImg,getDefenderHp;
+    var baseAP,getDefenderName,getDefenderImg,getDefenderHp;
     
-
-    $('#avatar1').find('p:last').text('HP:' + combatPoints[0].hp);
-    $('#avatar2').find('p:last').text('HP:' + combatPoints[1].hp);
-    $('#avatar3').find('p:last').text('HP:' + combatPoints[2].hp);
-    $('#avatar4').find('p:last').text('HP:' + combatPoints[3].hp);
+    for(i=0;i<combatPoints.length;i++){
+        var id = "#avatar";
+        var child = 1 + i;
+        $(id + child).find('p:last').text('HP:' + combatPoints[i].hp);
+    }
+//    $('#avatar1').find('p:last').text('HP:' + combatPoints[0].hp);
+//    $('#avatar2').find('p:last').text('HP:' + combatPoints[1].hp);
+//    $('#avatar3').find('p:last').text('HP:' + combatPoints[2].hp);
+//    $('#avatar4').find('p:last').text('HP:' + combatPoints[3].hp);
     $('#challengeZone').hide();
     $('#defenderZone').hide();
     $('#attack').hide();
@@ -29,7 +31,7 @@ $(document).ready(function(){
     $('#score').hide();
     $('#msg').hide();
     
-    $('.card').on('click',function(){
+    $('.main').on('click',".card",function(){
         //check if challenger or defender
         if (!challenger){
             challenger = true;
@@ -45,35 +47,32 @@ $(document).ready(function(){
             $('#challengerHp').text(getChallengerHp);
             $('#challengerImg').attr('src',getChallengerImg);
             $('#challengeZone').show();
+            $('#reset').show();
     
             getChallengerpt = getCombatAp(getChallengerName);
+            baseAP = getChallengerpt.attackerAp;
         } else  {
-//            if(defeated <= combatPoints.length -1)
+            $('.main').hide();
             $('#msg').hide();
             defeated += 1;
-           // defender = true;
+           
             console.log("defeated - ",defeated);
             getDefenderId = $(this).attr('id');
-//            console.log(getDefenderId);
+
             getDefenderName = $(this).find("p:first").text();
             getDefenderImg = $(this).find('img').attr('src');
             getDefenderHp = $(this).find("p:last").text();
             $(this).hide();
-//            $('#attack').css('display','block');
+
             $('#attack').show();
-            $('#reset').show();
+            $("#attack").removeAttr("disabled");
+            
             $('#defenderName').text(getDefenderName);
             $('#defenderHp').text(getDefenderHp);
             $('#defenderImg').attr('src',getDefenderImg);
             $('#defenderZone').show();
-//            $('#defenderZone').css('display','block');
+
             getDefenderpt = getCombatAp(getDefenderName);
-//            console.log(getDefenderName);
-//            console.log(getDefenderHp);
-//            console.log(getDefenderImg);
-//            $(".card").off("click");
-//            $(".card").on("click");
-            //remove clicks on rest of the defenders
             
         }
     });
@@ -81,11 +80,7 @@ $(document).ready(function(){
        
        
         console.log(!challengerAp);
-      //  if(!challengerAp){
-//            console.log("challengerap");
-//            console.log(getChallengerId);
-//            console.log(getChallengerpt);
-//            console.log(getDefenderpt);
+      
             if(getChallengerpt.attackerHp > 0){
                 //update challenger HP
                 getChallengerpt.attackerHp = getChallengerpt.attackerHp - getDefenderpt.attackerAp;
@@ -95,24 +90,15 @@ $(document).ready(function(){
                 $('#defenderHp').text("HP :" +getDefenderpt.attackerHp );
                 $('.defendend').text(getDefenderName); $('#yourDamage').text(getChallengerpt.attackerAp);
                 $('#defendDamage').text(getDefenderpt.attackerAp);
-                getChallengerpt.attackerAp = getChallengerpt.attackerAp * 2;
+                
+                getChallengerpt.attackerAp = getChallengerpt.attackerAp + baseAP;
                 attacks ++;
                 $('#score').show();
-//                console.log(attacks);
-//                console.log(getChallengerpt.attackerHp);
+
                 checkWinner(getChallengerpt.attackerHp,getDefenderpt.attackerHp)
                 
             } 
-//            if(getDefenderpt.attackerHp > 0){
-//                console.log(getDefenderpt.attackerHp > 0);
-//                getDefenderpt.attackerHp = getDefenderpt.attackerHp - getChallengerpt.attackerAp;
-//                
-//                $('#defenderHp').text("HP :" +getDefenderpt.attackerHp );
-//                $('.defendend').text(getDefenderName); $('#yourDamage').text(getChallengerpt.attackerAp);
-//                getChallengerpt.attackerAp = getChallengerpt.attackerAp * 2;
-////                $('#score').show();
-//                //checkWinner();
-//            } 
+
         
     });
     function getCombatAp(attacker){
@@ -121,39 +107,37 @@ $(document).ready(function(){
                 attackerAp = combatPoints[i].ap;              
                 attackerHp = combatPoints[i].hp;
                 return {attackerAp : attackerAp,attackerHp: attackerHp};
-//                console.log(attackerAp)
-//                console.log(attackerHp);
+
             }
             }
     }
     function checkWinner(challengerHP,defenderHP){
         console.log(challengerHP);
         if (challengerHP <= 0){
-            console.log("inside winner")
-            $('#msg1').text('You lost the game!');
+            
+            $('#msg1').text('You lost the throne!');
             $('#msg').show();
              $('#choice').text('Game Over!!')
             $(".card").off("click");
             $("#attack").off("click");
             
             } else if( (defeated == 3)) {
-            $('#msg1').text('You won the game!');
+            $('#msg1').text('You won the throne!');
+            $("#msg1").append("<img src='assets/images/throne-of-swords.jpg' width='50px'>");
             $('#msg').show();
-            $('#choice').text('Game Over!!')
+            $('#choice').text('Game Over!!').jpg
             $(".card").off("click");
             $("#attack").off("click");
             
         } else if((defenderHP <= 0) && (defeated != 3)){
-            console.log(defeated)
-           // $(".card").on("click");
-           // $("#attack").on("click");
+            
             defender = false;
             $("#defenderZone").hide();
             $('#msg1').text('Choose your next defender!');
-            $('#msg').show();
+            $('.main').show();
+            $('#msg').show(); 
+            $("#attack").attr("disabled", "disabled");
             
-            //$("#attack").off("click");
-            console.log("defender <=0")
         } 
     }
     $("#reset").click(function(){
